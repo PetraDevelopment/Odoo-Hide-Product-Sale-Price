12746bf250dca8c5397486c33811da4c219a11fa
Sandraraouf2023 <s.sedik@t-petra.com>
2024-03-18 14:57:25 +0200

    Version 16

--- hide_price/__manifest__.py
+++ hide_price/__manifest__.py
@@ -1,9 +1,11 @@
 {
-    'name':'Hide product price',
-    'summary':'show or hide cost or sale price',
+    'name':'Hide Product Price',
+    'summary':'Show Or Hide Cost Or Sale price',
     'depends': ['base', 'product'],
     'category':'Purchase',
-    'author':'Petra Software Industry',
+    'author':'Petra Software',
+    'company': 'Petra Software',
+    'maintainer': 'Petra Software',
       'website':'www.t-petra.com',
        'license': 'LGPL-3',
     'data':['security/hide_price_group.xml',
@@ -15,8 +17,8 @@
             'views/tree_view.xml'],
             
         
-        'images': ['static/description/icon.png'],
-              'price':8,
+        'images': ['static/description/banner.png'],
+              'price':10,
     'currency':'USD'
 
 
